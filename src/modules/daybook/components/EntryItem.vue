5c6553aa3e1529ca8e9fa114fcc957fb9fbfffde
Cristian Ramirez <cristianramirezgt@gmail.com>
2022-05-25 08:53:15 -0600

    20220525 085315
    Add entry view component

--- src/modules/daybook/components/EntryItem.vue
+++ src/modules/daybook/components/EntryItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="entry-container mb-3 pointer p-2">
+  <div class="entry-container mb-3 pointer p-2" @click="$router.push({ name: 'entry', params: { id: 10 } })">
     <!-- Title -->
     <div class="entry-title d-flex">
       <span class="text-success fs-5 fw-bold">25</span>
