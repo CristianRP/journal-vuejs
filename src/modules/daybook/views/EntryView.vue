811b1be59fe0910d8e8fbee210929d372d8d5277
Cristian Ramirez <cristianramirezgt@gmail.com>
2022-05-26 07:46:59 -0600

    20220526 074659
    Add validations to entry view

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-      <span class="text-success fs-3 fw-bold">{{ day }}</span>
-      <span class="mx-1 fs-3">{{ month }}</span>
-      <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+  <template v-if="entry">
+      <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+        <span class="text-success fs-3 fw-bold">{{ day }}</span>
+        <span class="mx-1 fs-3">{{ month }}</span>
+        <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+      </div>
+
+      <div>
+        <button class="btn btn-danger mx-2">
+          Delete
+          <i class="fa fa-trash-alt"></i>
+        </button>
+        <button class="btn btn-primary">
+          Upload Photo
+          <i class="fa fa-upload"></i>
+        </button>
+      </div>
     </div>
+    <hr>
+    <!-- Need to add a template to wrap both root -->
+    <div class="d-flex flex-column px-3 h-75">
+      <textarea placeholder="What happened today?" v-model="entry.text"></textarea>
 
-    <div>
-      <button class="btn btn-danger mx-2">
-        Delete
-        <i class="fa fa-trash-alt"></i>
-      </button>
-      <button class="btn btn-primary">
-        Upload Photo
-        <i class="fa fa-upload"></i>
-      </button>
+      <FabButton icon="fa-save"/>
+
+      <img
+        src="https://elsolnewsmedia.com/wp-content/uploads/2020/10/1601498876_582320_1601498920_noticia_normal.jpg"
+        alt="entry-picture"
+        class="img-thumbnail">
     </div>
-  </div>
-  <hr>
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea placeholder="What happened today?" v-model="entry.text"></textarea>
-
-    <FabButton icon="fa-save"/>
-
-    <img
-      src="https://elsolnewsmedia.com/wp-content/uploads/2020/10/1601498876_582320_1601498920_noticia_normal.jpg"
-      alt="entry-picture"
-      class="img-thumbnail">
-  </div>
+  </template>
 </template>
 
 <script>
@@ -79,15 +82,15 @@ export default {
   methods: {
     loadEntry() {
       const entry = this.getEntryById(this.id)
-      if ( !entry ) this.$router.push({ name: 'no-entry' })
+      if ( !entry ) return this.$router.push({ name: 'no-entry' })
 
       this.entry = entry
     }
   },
   watch: {
     // eslint-disable-next-line no-unused-vars
-    id(newValue, _oldValue) {
-      this.loadEntry(newValue)
+    id() {
+      this.loadEntry()
     }
   }
 }
