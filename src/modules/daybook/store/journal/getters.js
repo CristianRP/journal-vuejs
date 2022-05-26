22f0dc4f7b6d23850efa7f17bda877d983a698e9
Cristian Ramirez <cristianramirezgt@gmail.com>
2022-05-26 06:32:50 -0600

    20220526 063250
    Add store module on daybook

---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,13 @@
+// export const myGetter = (state) => {
+  // return state
+// }
+
+export const getEntrieByTerm = (/* state */) => {
+  // return state
+}
+
+export const getEntryById = (/* state */) => {
+  // return state
+}
+
+
