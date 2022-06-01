777c6d3d69addc4727868d59b9befabfb85cd2e4
Cristian Ramirez <cristianramirezgt@gmail.com>
2022-06-01 09:20:03 -0600

    20220601 092003

--- tests/unit/modules/daybook/router/index.spec.js
+++ tests/unit/modules/daybook/router/index.spec.js
@@ -28,4 +28,16 @@ describe("Tests on router module from Dayboo", () => {
     expect( routes ).toContain('EntryView')
     expect( routes ).toContain('NoEntrySelected')
   })
+
+  test('should return the id of the route', () => {
+    const route = {
+      params: {
+        id: 'ABC-123'
+      }
+    }
+
+    // expect( daybookRouter.children[1].props( route ) ).toEqual({ id: 'ABC-123' })
+    const entryRoute = daybookRouter.children.find( route => route.name === 'entry' )
+    expect( entryRoute.props(route) ).toEqual({ id: 'ABC-123' })
+  })
 })
