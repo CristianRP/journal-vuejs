ef3da57e699ed9cf277365734916bfc36696e467
Cristian Ramirez <cristianramirezgt@gmail.com>
2022-08-04 08:01:59 -0600

    20220804 080159
    Add auth guard for daybook routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 
 import authRouter from '@/modules/auth/router'
 import dayBookRouter from '@/modules/daybook/router'
+import isAuthenticatedGuard from '@/modules/auth/router/auth-guard'
 
 const routes = [
   {
@@ -24,6 +25,7 @@ const routes = [
   },
   {
     path: '/daybook',
+    beforeEnter: [ isAuthenticatedGuard ],
     ...dayBookRouter
   }
 ]
