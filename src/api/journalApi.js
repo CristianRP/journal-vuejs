88f8e08305abc7e2ccbb1c09ee18dc59cf520b0e
Cristian Ramirez <cristianramirezgt@gmail.com>
2022-08-04 11:38:41 -0600

    20220804 113841
    Add interceptors to axios requests

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -4,4 +4,13 @@ const journalApi = axios.create({
   baseURL: 'https://vue-demos-c3180-default-rtdb.firebaseio.com'
 })
 
+journalApi.interceptors.request.use( (config) => {
+
+  config.params = {
+    auth: localStorage.getItem('idToken')
+  }
+
+  return config
+})
+
 export default journalApi
