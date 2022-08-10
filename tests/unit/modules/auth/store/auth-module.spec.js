896abc922291207b4608243f5011426867c871fb
Cristian Ramirez <cristianramirezgt@gmail.com>
2022-08-10 08:28:16 -0600

    20220810 082816
    Add tests for actions auth

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import createVuexStore from '../../../mock-data/mock-store.js'
 
 
@@ -81,5 +82,64 @@ describe('Vuex: tests on auth-module', () => {
 
     expect( store.getters['auth/currentState'] ).toBe('authenticated')
     expect( store.getters['auth/userName'] ).toBe('Cristian')
+  })
+
+  // Actions
+  test('Actions: createUser - Existing user', async() => {
+    const store = createVuexStore({
+      status: 'not-authenticated',
+      user: null,
+      idToken: null,
+      refreshToken: null
+    })
+
+    const newUser = { name: 'Cristian', email: 'patito@test.com', password: '123123' }
+
+    const response = await store.dispatch('auth/createUser', newUser)
+
+    expect( response ).toEqual({ ok: false, message: 'EMAIL_EXISTS' })
+
+    const { status, user, idToken, refreshToken } = store.state.auth
+
+    expect(status).toBe('not-authenticated')
+    expect(user).toBeFalsy()
+    expect(idToken).toBeFalsy()
+    expect(refreshToken).toBeFalsy()
+
+  })
+
+  test('Actions: createUser signInUser - Save a new user', async() => {
+    const store = createVuexStore({
+      status: 'not-authenticated',
+      user: null,
+      idToken: null,
+      refreshToken: null
+    })
+
+    const newUser = { name: 'PatoT', email: 'patotest@test.com', password: '123123' }
+
+    // SignIn
+    await store.dispatch('auth/signInUser', newUser)
+    const { idToken } = store.state.auth
+
+    // Delete user
+    const deleteResponse = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:delete?key=${process.env.VUE_APP_GOOGLE_API_KEY}`, {
+      idToken
+    })
+
+    console.log(deleteResponse);
+
+    // Create User
+    const response = await store.dispatch('auth/createUser', newUser)
+
+    console.log(response);
+    expect( response ).toEqual({ ok: true })
+
+    const { status, user, idToken:token, refreshToken } = store.state.auth
+
+    expect(status).toBe('authenticated')
+    expect(user).toMatchObject( { name: 'PatoT', email: 'patotest@test.com' } )
+    expect( typeof idToken).toBe( 'string' )
+    expect( typeof refreshToken).toBe( 'string' )
   })
 })
